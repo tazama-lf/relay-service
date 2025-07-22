30b9d247cb43bea9f5eb07ac26bfad95a4027843
rtkay123 <70331483+rtkay123@users.noreply.github.com>
2025-07-22 15:10:22 +0200

    build(lint): upgraded eslint and fix linting issues (#66)
    * fix: update ESLint configuration and plugins for improved linting rules
    
    * refactor: update import statements to use node built-in modules
    
    * refactor: update eslint and prettier configurations and dependencies
    
    - Simplified eslint fix command to run without specifying file patterns.
    - Updated eslint linting commands to run without file patterns.
    - Upgraded @stylistic/eslint-plugin to version 5.1.0.
    - Updated eslint-plugin-eslint-comments to version 4.5.0.
    - Upgraded @typescript-eslint/eslint-plugin and parser to version 8.35.1.
    - Updated eslint to version 9.30.1 and eslint-config-love to version 121.0.0.
    - Added eslint-config-prettier to ensure compatibility with prettier.
    
    * test: update transport plugin tests to use node built-in execSync
    
    * fix: improve error logging in initTransport by using util.inspect
    
    * refactor: bump frms-coe-lib version
    
    * build(deps): update dependencies
    
    ---------
    
    Co-authored-by: cshezi <cshezi@sybrin.co.za>
    Co-authored-by: rtkay123 <rodney.kanjala@sybrin.com>

--- src/utils/loadTransportPlugin.ts
+++ src/utils/loadTransportPlugin.ts
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
-import { ITransportPlugin } from '@tazama-lf/frms-coe-lib/lib/interfaces/relay-service/ITransportPlugin';
-import { LoggerService } from '@tazama-lf/frms-coe-lib';
-import { Apm } from '@tazama-lf/frms-coe-lib/lib/services/apm';
+import type { ITransportPlugin } from '@tazama-lf/frms-coe-lib/lib/interfaces/relay-service/ITransportPlugin';
+import type { LoggerService } from '@tazama-lf/frms-coe-lib';
+import type { Apm } from '@tazama-lf/frms-coe-lib/lib/services/apm';
 
 export const loadTransportPlugin = async (pluginName: string, loggerService: LoggerService, apm: Apm): Promise<ITransportPlugin> => {
   try {
     // Dynamically import the plugin
     loggerService.log(`Loading plugin ${pluginName}`);
-    const { default: moduleDefault } = await import(pluginName);
+    const { default: moduleDefault } = (await import(pluginName)) as { default: { default: new () => ITransportPlugin } };
     const PluginInstance = moduleDefault.default;
     return new PluginInstance();
   } catch (error) {
